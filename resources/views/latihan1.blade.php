49a0dc6ba2fcca659967570db0897d0b7fdfa0da
andri arfianto <aarfianto11@gmail.com>
2021-06-22 14:40:56 +0700

    routing home to view

---
+++ resources/views/latihan1.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Laravel Vue - SPA</title>
+</head>
+<style>
+	body {
+		font-family: sans-serif;
+		margin: 15%;
+	}
+</style>
+<body>
+	<div id="app"></div>
+	<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
